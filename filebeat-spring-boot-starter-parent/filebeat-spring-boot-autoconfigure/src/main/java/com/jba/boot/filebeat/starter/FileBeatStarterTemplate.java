02d4dd4eb414844fd2906f13464441bb36daa937
Jude Antony:q! <judebantony@gmail.com>
2019-06-22 18:10:34 +0530

    only supported for linux now

--- filebeat-spring-boot-starter-parent/filebeat-spring-boot-autoconfigure/src/main/java/com/jba/boot/filebeat/starter/FileBeatStarterTemplate.java
+++ filebeat-spring-boot-starter-parent/filebeat-spring-boot-autoconfigure/src/main/java/com/jba/boot/filebeat/starter/FileBeatStarterTemplate.java
@@ -10,6 +10,7 @@ import com.jba.boot.filebeat.utils.FileBeatConfigGenerator;
 import com.jba.boot.filebeat.utils.FileBeatDownloader;
 import com.jba.boot.filebeat.utils.FileBeatInstaller;
 import com.jba.boot.filebeat.utils.FileBeatProcessStarter;
+import com.jba.boot.filebeat.utils.OSInfo;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -26,29 +27,30 @@ public class FileBeatStarterTemplate {
 
 	@Autowired
 	private FileBeatInstaller fileBeatInstaller;
-	
+
 	@Autowired
 	private FileBeatConfigGenerator fileBeatConfigGenerator;
-	
+
 	@Autowired
 	private FileBeatProcessStarter fileBeatProcessStarter;
 
 	public FileBeatProcessContext startFileBeat() {
 		FileBeatProcessContext fileBeatProcessContext = new FileBeatProcessContext();
 		try {
-			fileBeatDownloader.downloadFileBeat();
-			fileBeatInstaller.installFileBeat();
-			fileBeatConfigGenerator.createFileBeatConfig();
-			fileBeatProcessContext.setProcessId(fileBeatProcessStarter.startFileBeat());
-			String message = String.format("Filebeat Stated with pid = %s and processing the log file %s",
-					fileBeatProcessContext.getProcessId(), fileBeatProcessContext.getLogFile());
-			fileBeatProcessContext.setMessage(message);
+			if (OSInfo.isUnix()) {
+				fileBeatDownloader.downloadFileBeat();
+				fileBeatInstaller.installFileBeat();
+				fileBeatConfigGenerator.createFileBeatConfig();
+				fileBeatProcessContext.setProcessId(fileBeatProcessStarter.startFileBeat());
+				String message = String.format("Filebeat Stated with pid = %s and processing the log file %s",
+						fileBeatProcessContext.getProcessId(), fileBeatProcessContext.getLogFile());
+				fileBeatProcessContext.setMessage(message);
+			}
 		} catch (Exception e) {
 			log.error(e.getMessage());
 			String message = String.format("Fail to start or install Filebeat %s", e.getMessage());
 			fileBeatProcessContext.setMessage(message);
 		}
 		return fileBeatProcessContext;
-
 	}
 }
---
+++ filebeat-spring-boot-starter-parent/filebeat-spring-boot-autoconfigure/src/main/java/com/jba/boot/filebeat/utils/OSInfo.java
@@ -0,0 +1,80 @@
+/**
+ * 
+ */
+package com.jba.boot.filebeat.utils;
+
+import java.io.IOException;
+import java.util.Locale;
+
+/**
+ * @author Jude
+ *
+ */
+public final class OSInfo {
+	
+	public enum OS {
+		WINDOWS, UNIX, SOLARIS, MAC, POSIX_UNIX, OTHER;
+
+		private String version;
+
+		public String getVersion() {
+			return version;
+		}
+
+		public void setVersion(String version) {
+			this.version = version;
+		}
+	}
+
+	private static OS os = OS.OTHER;
+
+	static {
+		try {
+			String osName = System.getProperty("os.name");
+			if (osName == null) {
+				throw new IOException("os.name not found");
+			}
+			osName = osName.toLowerCase(Locale.ENGLISH);
+			if (osName.contains("windows")) {
+				os = OS.WINDOWS;
+			} else if (osName.contains("linux") || osName.contains("mpe/ix") || osName.contains("freebsd")
+					|| osName.contains("irix") || osName.contains("digital unix") || osName.contains("unix")
+					|| osName.contains("aix")) {
+				os = OS.UNIX;
+			} else if (osName.contains("mac os")) {
+				os = OS.MAC;
+			} else if (osName.contains("sun os") || osName.contains("sunos") || osName.contains("solaris")) {
+				os = OS.SOLARIS;
+			} else if (osName.contains("hp-ux")) {
+				os = OS.POSIX_UNIX;
+			} else {
+				os = OS.OTHER;
+			}
+		} catch (Exception ex) {
+			os = OS.OTHER;
+		} finally {
+			os.setVersion(System.getProperty("os.version"));
+		}
+	}
+
+	public static OS getOs() {
+		return os;
+	}
+
+	public static boolean isWindows() {
+		return (getOs() == OS.WINDOWS);
+	}
+
+	public static boolean isMac() {
+		return (getOs() == OS.MAC);
+	}
+
+	public static boolean isUnix() {
+		return (getOs() == OS.UNIX);
+	}
+
+	public static boolean isSolaris() {
+		return (getOs() == OS.SOLARIS);
+	}
+
+}
